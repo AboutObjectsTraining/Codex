fe4b59365811d562062fb2dbdb973befd6651f7a
Jonathan Lehr <jonathan@aboutobjects.com>
2015-11-30 10:53:29 -0500

    Initial Commit

---
+++ Codex/Codex.h
@@ -0,0 +1,19 @@
+//
+//  Codex.h
+//  Codex
+//
+//  Created by Jonathan on 11/30/15.
+//  Copyright © 2015 About Objects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Codex.
+FOUNDATION_EXPORT double CodexVersionNumber;
+
+//! Project version string for Codex.
+FOUNDATION_EXPORT const unsigned char CodexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Codex/PublicHeader.h>
+
+
